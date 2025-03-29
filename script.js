beb61821300a425cf03dc866a918f88d75ab4aed
nusima-59487 <wongktoliver@gmail.com>
2025-03-29 12:07:06 -0700

    Made it a class object

--- script.js
+++ script.js
@@ -1,71 +1,166 @@
-// fetch from https://raw.githubusercontent.com/Purukitto/pokemon-data.json/refs/heads/master/pokedex.json
-var score = 0;
-var data; 
-fetch("https://raw.githubusercontent.com/Purukitto/pokemon-data.json/refs/heads/master/pokedex.json")
-  .then((response) => response.json())
-  .then((ddd) => {
-    data = ddd; 
-    console.log(data);
-  });
+async function fetchData() {
+    var toreturn; 
+    await fetch("https://raw.githubusercontent.com/Purukitto/pokemon-data.json/refs/heads/master/pokedex.json")
+        .catch((error) => {
+            console.error('Error from fetching data:', error);
+            alert("An error occured. Please try again later.");
+            return;
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            toreturn = data; 
+        });
+    return toreturn; 
+}; 
 
-function randomize() {
-  let random = Math.floor(Math.random() * data.length);
-  console.log(data[random]);
-  return data[random];
-}
+class game {
+    constructor (data) {
+        this.score = 0; 
+        this.data = data; // data from the fetch
+        this.entry = null; 
+        this.isGameOver = false;
 
-function nextEntry() {
-    htmlImg = document.getElementById("mainimg"); 
-    htmlName = document.getElementById("pokemonname");
+        // might use constructor params
+        this.htmlImg = document.getElementById("mainimg");
+        this.htmlName = document.getElementById("pokemonname");
+        this.htmlInput = document.getElementById("userinput");
+        this.htmlScore = document.getElementById("score");
+        this.htmlHigh = document.getElementById("high");
+        this.htmlInputDiv = document.getElementById("userinputdiv");
+        this.htmlGiveupDiv = document.getElementById("usergiveupdiv");
 
-    ent = randomize();
-    entImg = ent["image"]["hires"];
-    entName = ent["name"]["english"];
-    htmlImg.src = entImg;
-    htmlName.innerHTML = entName;
-}
-
-/**
- * Get the high score from localstorage
- * @returns {number} the high score
- */
-function getHighScore () {
-    s = localStorage.getItem('highscore')
-    if (s == null) {
-        localStorage.setItem('highscore', 0);
-        return 0;
+        this.stats = {
+            // todo
+        }
     }
-    return s;
-}
 
-/**
- * Set the high score to localstorage
- * @param {number} score score to set as high score
- * @returns {boolean} true if successful, false if score lower than current high score
- */
-function setHighScore (score) {
-    let currentHighScore = getHighScore();
-    if (score < currentHighScore) {
-        return false;
+    /**
+     * Gets random entry from `game.data` 
+     * @returns {object} random entry from the data
+     */
+    random() {
+        let random = Math.floor(Math.random() * this.data.length);
+        // console.log(this.#data[random]);
+        return this.data[random];
     }
-    localStorage.setItem('highscore', score);
-    return true; 
+
+    /**
+     * Get the next entry from the data and display it
+     * 
+     * [only if game in progress]
+     */
+    nextEntry() {
+        if (this.isGameOver) return; // if gameover, do nothing    
+        this.entry = this.random();
+        let entImg = this.entry["image"]["hires"];
+        this.htmlImg.src = entImg;
+
+        // show score and reset input box
+        this.htmlScore.innerText = this.score;  
+        this.htmlHigh.innerText = this.getHighScore();  
+        this.htmlInput.value = ""; 
+        this.htmlInput.focus(); // focus on input box
+
+        // plz dont
+        console.log(this.entry["name"]["english"]);
+    }
+
+    /**
+     * Checks the answer from `#userinput`
+     * 
+     * [only if game in progress]
+     */
+    checkAnswer() {
+        if (this.isGameOver) return; // if gameover, do nothing
+        let input = this.htmlInput.value.toLowerCase();
+        let entName = this.entry["name"]["english"].toLowerCase();
+
+        if (input == entName) {
+            this.score++;
+            let a = this.setHighScore(score);
+
+            this.nextEntry();
+        } else {
+            this.endgame(); 
+        }
+    }
+
+    /**
+     * Ends the game
+     * @returns {string} the name of the entry
+     */
+    endgame () {
+        let entName = this.entry["name"]["english"];
+        this.isGameOver = true; 
+        this.htmlInputDiv.hidden = true;
+        this.htmlGiveupDiv.hidden = false;
+        this.htmlName.innerText = entName;
+        return entName;
+    }
+
+    /**
+     * @todo
+     */
+    skipEntry () {
+        // TODO
+    }
+
+    retry () {
+        this.isGameOver = false; 
+        this.htmlInputDiv.hidden = false;
+        this.htmlGiveupDiv.hidden = true;
+        this.htmlName.innerHTML = "";
+        this.score = 0;
+        this.htmlScore.innerText = this.score;    
+
+        return this.nextEntry();
+    }
+
+    /**
+     * Get the high score from `localStorage`, set 0 if null
+     * @returns {number} the high score
+     */
+    getHighScore () {
+        let s = localStorage.getItem('highscore')
+        if (s == null) {
+            localStorage.setItem('highscore', 0);
+            return 0;
+        }
+        return s;
+    }
+
+    /**
+     * Set the high score to `localStorage`
+     * @returns {boolean} true if successful, false if score lower than current high score
+     */
+    setHighScore () {
+        let currentHighScore = this.getHighScore();
+        if (this.score < currentHighScore) {
+            return false;
+        }
+        localStorage.setItem('highscore', this.score);
+        return true; 
+    }
+
 }
 
 
 // Button functions mapping
 function btnUserConfirm() {
-
+    g1.checkAnswer();
 }
 
 function btnUserGiveup () {
-
+    g1.endgame(); 
 }
 
 function btnUserSkip () {
-
+    g1.skipEntry(); 
 }
 
 function btnRetry () {
-
+    g1.retry();
 }
+
+
+
